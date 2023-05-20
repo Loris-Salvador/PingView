54b451bba6750c02d9ca17f23bd875450c1c2b98
Loris Salvador <loris.salvador@student.hepl.be>
2023-05-20 23:43:30 +0200

    Add project files.

---
+++ Infrastructure/WrapperBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public abstract class WrapperBase : BindableBase
+    {
+        public object Content { get; set; }
+
+
+        protected WrapperBase(object content)
+        {
+            Content = content;
+        }
+    }
+}
